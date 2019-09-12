8a3cd1f0bf9fe30e53078163aa8d481616fc3e99
Liuhui666666 <615834393@qq.com>
2019-09-12 15:07:57 +0800

    qwer

---
+++ jgsspringday05_01/src/test/java/TestSpring.java
@@ -0,0 +1,22 @@
+import cn.kgc.spring.impl.A4;
+import cn.kgc.spring.impl.Gray;
+import cn.kgc.spring.print.Print;
+import org.junit.Test;
+
+/**
+ * Created with IDEA
+ * author:bdqn kgc zhou qi chao
+ * Date:2019/9/2
+ * Time:15:06
+ */
+public class TestSpring {
+    /*@Test
+    public void test01(){
+        Print print = new Print();
+
+        print.setInkBox(new Gray());
+        print.setPaper(new A4());
+
+        print.show();
+    }*/
+}
